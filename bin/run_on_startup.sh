8e80e4b86d512f414d74a9f7d0d2b86c1cf1b60a
Eilliw <36427389+Eilliw@users.noreply.github.com>
2023-12-04 01:54:25 -0700

    eddited startup scripts

--- bin/run_on_startup.sh
+++ bin/run_on_startup.sh
@@ -4,5 +4,6 @@ DISPLAY=:0 xterm -hold -e /home/remote/Dev/Procam
 #sudo killall pigpiod
 sudo pigpiod
 cd /home/project/GEEN1400-Final-Project
+cd /home/project/trash-classification-public
 source venv/bin/activate
 python3 scripts/button.py
