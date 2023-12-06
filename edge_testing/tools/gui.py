33a75890e97f5ce9ac35ea9a4fa833f1bd7d9f1f
Eilliw <36427389+Eilliw@users.noreply.github.com>
2023-12-06 02:23:24 -0700

    functional with threading Event flag

--- edge_testing/tools/gui.py
+++ edge_testing/tools/gui.py
@@ -3,15 +3,17 @@ from tkinter.ttk import *
 from PIL import Image, ImageTk
 import numpy as np
 import cv2
-from dataset import PiCamDataset
-from threading import Condition
+# from dataset import PiCamDataset
+from  threading import Event
+
 class TestingGUI:
-    def __init__(self, dataset: PiCamDataset) -> None:
+    def __init__(self, dataset, image_flag : Event) -> None:
         self.root = Tk()
         self.root.title("Testing gui")
         self.root.geometry("360x360")
 
         self.dataset = dataset
+        self.image_flag = image_flag
 
         self.canvas = Canvas(self.root, height=224, width=224, bg='gray')
         self.canvas.grid(row=0, column=1, rowspan=4, columnspan=4)
@@ -25,12 +27,17 @@ class TestingGUI:
         self.label_style = Style()
         self.label_style.configure("DF.TLabel", foreground="white", background="grey")
 
-        self.buttons()
+        self.buttons_labels()
+
     
-    def buttons(self):
+    def buttons_labels(self):
         self.btn_label_text = "Click the button based on what the image is"
         self.btn_label = Label(self.root, text=self.btn_label_text)
         self.btn_label.grid(row=5, column = 0, columnspan=4)
+
+        self.classification_label_text = 'Classification: '
+        self.classification_label =  Label(self.root, text=self.classification_label_text)
+        self.classification_label.grid(row=6, column=0)
 
         self.trash_button = Button(self.root, text="Trash",style="DF.TButton", command=self._trash_button_callback)
         self.trash_button.grid(row=1, column=0)
@@ -44,14 +51,20 @@ class TestingGUI:
 
 
     def set_canvas_img(self, img :  np.ndarray):
+
         if isinstance(img, np.ndarray):
             res = cv2.resize(img, dsize=(224,224), interpolation=cv2.INTER_CUBIC)
             self.canvas_img = ImageTk.PhotoImage(image=Image.fromarray(res))
             self.img = Image.fromarray(res)
             self.canvas.create_image(0,0, anchor = 'nw', image=self.canvas_img)
+            # acquire condtion lock
+            
         else:
             raise TypeError("needs  to be ndarray")
+        self.image_flag.clear()
         self.buttons_ready()
+    def set_classification(self, classification: tuple[str, float]):
+        self.classification_label.configure(text=self.classification_label_text+classification[0]+"-"+classification[1])
     def set_button_state(self, state,  style=None):
         #state  must be normal  or  disabled
         for btn in [self.trash_button, self.recycle_button,  self.upload_button]:
@@ -61,16 +74,33 @@ class TestingGUI:
     def after_button_press(self):
         self.set_button_state("disabled", style="Pressed.TButton")
         self.btn_label.configure(text="Waiting for image and classification to load")
+        print("notifying threads")
+        #notifys thread waiting on this condition
+        self.image_flag.set()
+
+
     def buttons_ready(self):
         self.set_button_state("normal", style="DF.TButton")
+
+
+    def _on_close(self):
+        self.dataset.upload_dirs(['lib/test_collection/Recycle', "lib/test_collection/Trash"], with_annotations=True)
+        self.root.destroy()
+
+
     def _trash_button_callback(self):
         self.btn_label.configure(text="Click the button based on what the image is")
+        self.dataset.save_to_local(self.img, "lib/test_collection/Trash", "Trash-%s.jpg")
         self.after_button_press()
+
     def _recycle_button_callback(self):
+        self.btn_label.configure(text="Click the button based on what the image is")
+        self.dataset.save_to_local(self.img, "lib/test_collection/Recycle", "Recycle-%s.jpg")
         self.after_button_press()
+
     def _upload_button_callback(self):
         self.after_button_press()
-        self.dataset.upload_dirs("lib/test_collection")
+        self.dataset.upload_dirs(["lib/test_collection/Recycle","lib/test_collection/Trash"])
 
 if __name__ == "__main__":
 
@@ -79,4 +109,7 @@ if __name__ == "__main__":
 
     image = Image.open("lib/test_collection/Trash/IMG_4283 Large.jpg")
     window.set_canvas_img(np.asarray(image))
+
+
+    window.root.protocol("WM_DELETE_WINDOW", window._on_close)
     window.root.mainloop()
