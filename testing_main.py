c4b784b5f7e4ceab3109a0b7715c2945fd90e61b
Eilliw <36427389+Eilliw@users.noreply.github.com>
2023-12-06 10:21:40 -0700

    added linux image thread and made darwin image stream working

--- testing_main.py
+++ testing_main.py
@@ -1,11 +1,14 @@
 from edge_testing.tools.dataset import PiCamDataset
 from edge_testing.tools.gui import TestingGUI
 from triton_client.inference  import InferenceClient
+from PIL import Image
 import dotenv, sys, cv2, threading,  time
 
 
 dotenv.load_dotenv(".env")
 
+def get_client():
+    return InferenceClient("192.168.191.129", "yolov8x-cls", "grpc", labels='lib/labels/yolov8-7classes.txt')
 
 def crop_center(img,cropx,cropy):
     y,x,c = img.shape
@@ -14,46 +17,88 @@ def crop_center(img,cropx,cropy):
     return img[starty:starty+cropy,startx:startx+cropx, :]
 
 
-def linux_image_stream():
-    pass
-def mac_image_stream(client  ,window: TestingGUI ,flag: threading.Event):
+def linux_image_stream(window: TestingGUI, flag: threading.Event):
+    #initilise client
+    client = get_client()
+    #client = httpclient.InferenceServerClient("192.168.191.129:8001")
+    print("made client")
+    print(client.is_server_live())
+    picam2 = Picamera2()
+    
+    still_config = picam2.create_still_configuration(main={"size": (640,640), "format":'RGB888'})
+    picam2.configure(still_config)
+    picam2.start()
+    time.sleep(1)
+    while not window.exit_flag.is_set():
+        flag.wait()
+        try:
+            frame = picam2.capture_array("main")
+            print(frame.shape)
+                
+            cropped = client.crop_center(frame, 640,640)
+            model_infer = client.call(cropped)
+
+            #match classification
+            output = client.match_class(model_infer[0])
+            print(output)
+
+            #send  classification  to window
+            window.set_classification(output)
+            #send  img to  window - this causes the window to aquire the lock
+            window.set_canvas_img(cropped)
+        except RuntimeError:
+            print("Runtime error reached, most likely on purpose")
+    picam2.stop()
+    client.close()
+def mac_image_stream(window: TestingGUI ,flag: threading.Event):
+    #initilise client
+    client = get_client()
+    #client = httpclient.InferenceServerClient("192.168.191.129:8001")
+    print("made client")
+    print(client.is_server_live())
+
     cap = cv2.VideoCapture(0, cv2.CAP_ANY)
     time.sleep(2)
     if cap.isOpened():
         print("cap is opened")
         ret, frame = cap.read()
-        cropped = crop_center(frame, 640,640)
-        window.set_canvas_img(cropped)
-        while True:
+        # cropped = crop_center(frame, 640,640)
+        # window.set_canvas_img(cropped)
+        while not window.exit_flag.is_set():
             flag.wait()
+            try:
 
-            #get image from camera
-            ret, frame = cap.read()
-            if ret:
-                print(frame.shape)
+                #get image from camera
+                ret, frame = cap.read()
+                if ret:
+                    print(frame.shape)
+                    rgb_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    
 
-                cropped = crop_center(frame, 640,640)
-                #resized = cv2.resize(cropped, dsize=(224,224), interpolation=cv2.INTER_CUBIC)
-                ##model  infer
-                #model_infer = client.call(cropped)
+                    cropped = crop_center(rgb_img, 640,640)
+                    #cropped = cv2.resize(rgb_img, dsize=(224,224), interpolation=cv2.INTER_CUBIC)
+                    ##model  infer
+                    model_infer = client.call(cropped)
+                    print(model_infer)
 
-                ##match classification
-                #output = client.match_class(model_infer[0])
-                #print(output)
+                    #match classification
+                    output = client.match_class(model_infer[0])
+                    print(output)
 
-                ##send  classification  to window
-                #window.set_classification(output)
-                ##send  img to  window - this causes the window to aquire the lock
-                window.set_canvas_img(cropped)
+                    #send  classification  to window
+                    window.set_classification(output)
+                    #send  img to  window - this causes the window to clear the event flag
+                    window.set_canvas_img(cropped)
+            except RuntimeError:
+                print("Runtime error reached, most likely on purpose")
+        cap.release()
+        cv2.destroyAllWindows()
+        client.close()
     else:
         print("video not opened")
-if sys.platform == "linux":
-    from picamera2 import Picamera2
-elif  sys.platform == 'darwin':
-    client = InferenceClient("192.168.191.129", "yolov8l-cls", "grpc", labels='lib/labels/yolov8-7classes.txt')
-    #client = httpclient.InferenceServerClient("192.168.191.129:8001")
-    print("made client")
-    print(client.is_server_live())
+
+if __name__ == "__main__":
+    
 
     #get picam dataset
     dataset = PiCamDataset()
@@ -61,7 +106,11 @@ elif  sys.platform == 'darwin':
     cond = threading.Event()
 
     window = TestingGUI(dataset, cond)
-    image_stream = threading.Thread(target=mac_image_stream, args=(client, window, cond))
+    if sys.platform == "linux":
+        from picamera2 import Picamera2
+
+    elif sys.platform == 'darwin':
+        image_stream = threading.Thread(target=mac_image_stream, args=(window, cond))
 
     window.root.protocol("WM_DELETE_WINDOW", window._on_close)
     
@@ -69,4 +118,3 @@ elif  sys.platform == 'darwin':
 
     window.root.mainloop()
     image_stream.join()
-    client.close()
