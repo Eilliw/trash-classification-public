18ab9fc680decdcc91ff565a4c2a5e8d97187061
Eilliw <36427389+Eilliw@users.noreply.github.com>
2023-12-09 01:44:33 -0700

    added number of saved images check

--- scripts/button.py
+++ scripts/button.py
@@ -134,6 +134,7 @@ class InferenceButton():
                 print("waiting 10 seconds")
                 time.sleep(10)
                 img = Image(cv2.resize(image, dsize=(224, 224), interpolation=cv2.INTER_CUBIC))
+                if os.listdir("lib/unlabeled-imgs")
                 PiCamDataset.save_to_local(img, path="lib/unlabeled-imgs", path_pattern=None, sequential=False)
                 return
         except:
